a9f3da2ae06e82ee6d7fd4dd778685e7c608c440
stwe <no@mail.tt>
2021-09-07 22:47:47 +0200

    uniforms

--- src/main/java/de/sg/benno/ogl/resource/ShaderProgram.java
+++ src/main/java/de/sg/benno/ogl/resource/ShaderProgram.java
@@ -19,6 +19,7 @@ import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.EnumSet;
 import java.util.HashMap;
+import java.util.Scanner;
 
 import static de.sg.benno.ogl.Log.LOGGER;
 import static org.lwjgl.opengl.GL20.*;
@@ -32,6 +33,16 @@ import static org.lwjgl.opengl.GL40.GL_TESS_EVALUATION_SHADER;
  * Represents a shader program.
  */
 public class ShaderProgram implements Resource {
+
+    //-------------------------------------------------
+    // Constants
+    //-------------------------------------------------
+
+    private static final String VERTEX_SHADER_FILE_NAME = "/Vertex.vert";
+    private static final String TESSELLATION_CONTROL_SHADER_FILE_NAME = "/TessControl.tesc";
+    private static final String TESSELLATION_EVALUATION_SHADER_FILE_NAME = "/TessEval.tese";
+    private static final String GEOMETRY_SHADER_FILE_NAME = "/Geometry.geom";
+    private static final String FRAGMENT_SHADER_FILE_NAME = "/Fragment.frag";
 
     //-------------------------------------------------
     // Types
@@ -99,12 +110,12 @@ public class ShaderProgram implements Resource {
     private final ArrayList<Uniform> foundUniforms = new ArrayList<>();
 
     /**
-     *
+     * Stores Uniform locations.
      */
     private final HashMap<String, Integer> uniforms = new HashMap<>();
 
     /**
-     *
+     * Stores Uniform-block locations.
      */
     private final HashMap<String, Integer> uniformBlocks = new HashMap<>();
 
@@ -160,27 +171,27 @@ public class ShaderProgram implements Resource {
 
         // add VERTEX_SHADER
         if (options.contains(Options.VERTEX_SHADER)) {
-            addVertexShader(readFileIntoString(shaderPath + "/Vertex.vert"));
+            addVertexShader(readFileIntoString(shaderPath + VERTEX_SHADER_FILE_NAME));
         }
 
         // add TESSELLATION_CONTROL_SHADER
         if (options.contains(Options.TESSELLATION_CONTROL_SHADER)) {
-            addTessellationControlShader(readFileIntoString(shaderPath + "/TessControl.tesc"));
+            addTessellationControlShader(readFileIntoString(shaderPath + TESSELLATION_CONTROL_SHADER_FILE_NAME));
         }
 
         // add TESSELLATION_EVALUATION_SHADER
         if (options.contains(Options.TESSELLATION_EVALUATION_SHADER)) {
-            addTessellationEvaluationShader(readFileIntoString(shaderPath + "/TessEval.tese"));
+            addTessellationEvaluationShader(readFileIntoString(shaderPath + TESSELLATION_EVALUATION_SHADER_FILE_NAME));
         }
 
         // add GEOMETRY_SHADER
         if (options.contains(Options.GEOMETRY_SHADER)) {
-            addGeometryShader(readFileIntoString(shaderPath + "/Geometry.geom"));
+            addGeometryShader(readFileIntoString(shaderPath + GEOMETRY_SHADER_FILE_NAME));
         }
 
         // add FRAGMENT_SHADER
         if (options.contains(Options.FRAGMENT_SHADER)) {
-            addFragmentShader(readFileIntoString(shaderPath + "/Fragment.frag"));
+            addFragmentShader(readFileIntoString(shaderPath + FRAGMENT_SHADER_FILE_NAME));
         }
 
         linkAndValidateProgram();
@@ -343,6 +354,13 @@ public class ShaderProgram implements Resource {
     // Create Shader
     //-------------------------------------------------
 
+    /**
+     * Creates a shader with specified type.
+     *
+     * @param shaderType Type of the shader.
+     *
+     * @return The handle of the shader.
+     */
     private static int createShaderObject(int shaderType) {
         var shaderId = glCreateShader(shaderType);
         if (shaderId == 0) {
@@ -356,11 +374,17 @@ public class ShaderProgram implements Resource {
     // Helper
     //-------------------------------------------------
 
+    /**
+     * Read the shader code into a string.
+     *
+     * @param path The path to the shader file.
+     * @return The shader code as string.
+     * @throws FileNotFoundException If the file does not exist.
+     */
     private static String readFileIntoString(String path) throws FileNotFoundException {
-        String result = null;
+        String result;
 
-        /*
-        var in = ResourceManager.class.getResourceAsStream(path);
+        var in = ShaderProgram.class.getResourceAsStream(path);
         if (in == null) {
             throw new FileNotFoundException("Resource " + path + " not found.");
         }
@@ -368,41 +392,73 @@ public class ShaderProgram implements Resource {
         try (Scanner scanner = new Scanner(in, java.nio.charset.StandardCharsets.UTF_8.name())) {
             result = scanner.useDelimiter("\\A").next();
         }
-        */
 
         return result;
     }
 
+    /**
+     * Add a Vertex Shader.
+     *
+     * @param shaderCode The shader code.
+     */
     private void addVertexShader(String shaderCode) {
         LOGGER.debug("A Vertex Shader is added to program {}.", id);
         vertexShaderId = addShader(shaderCode, GL_VERTEX_SHADER);
         LOGGER.debug("A new Vertex Shader was added to program {}. The Id is {}.", id, vertexShaderId);
     }
 
+    /**
+     * Add a Tessellation Control Shader.
+     *
+     * @param shaderCode The shader code.
+     */
     private void addTessellationControlShader(String shaderCode) {
         LOGGER.debug("A Tessellation Control Shader is added to program {}.", id);
         tessellationControlShaderId = addShader(shaderCode, GL_TESS_CONTROL_SHADER);
         LOGGER.debug("A new Tessellation Control Shader was added to program {}. The Id is {}.", id, tessellationControlShaderId);
     }
 
+    /**
+     * Add a Tessellation Evaluation Shader.
+     *
+     * @param shaderCode The shader code.
+     */
     private void addTessellationEvaluationShader(String shaderCode) {
         LOGGER.debug("A Tessellation Evaluation Shader is added to program {}.", id);
         tessellationEvaluationShaderId = addShader(shaderCode, GL_TESS_EVALUATION_SHADER);
         LOGGER.debug("A new Tessellation Evaluation Shader was added to program {}. The Id is {}.", id, tessellationEvaluationShaderId);
     }
 
+    /**
+     * Add a Geometry Shader.
+     *
+     * @param shaderCode The shader code.
+     */
     private void addGeometryShader(String shaderCode) {
         LOGGER.debug("A Geometry Shader is added to program {}.", id);
         geometryShaderId = addShader(shaderCode, GL_GEOMETRY_SHADER);
         LOGGER.debug("A new Geometry Shader was added to program {}. The Id is {}.", id, geometryShaderId);
     }
 
+    /**
+     * Add a Fragment Shader.
+     *
+     * @param shaderCode The shader code.
+     */
     private void addFragmentShader(String shaderCode) {
         LOGGER.debug("A Fragment Shader is added to program {}.", id);
         fragmentShaderId = addShader(shaderCode, GL_FRAGMENT_SHADER);
         LOGGER.debug("A new Fragment Shader was added to program {}. The Id is {}.", id, fragmentShaderId);
     }
 
+    /**
+     * Attaches a shader object to this program.
+     *
+     * @param shaderCode The shader code.
+     * @param shaderType The shader type.
+     *
+     * @return The handle of the shader.
+     */
     private int addShader(String shaderCode, int shaderType) {
         var shaderId = createShaderObject(shaderType);
 
@@ -416,11 +472,22 @@ public class ShaderProgram implements Resource {
         return shaderId;
     }
 
+    /**
+     * Compiles a shader object.
+     *
+     * @param shaderId The handle of the shader.
+     * @param shaderCode The shader code.
+     */
     private static void compileShader(int shaderId, String shaderCode) {
         glShaderSource(shaderId, shaderCode);
         glCompileShader(shaderId);
     }
 
+    /**
+     * Checks compile status.
+     *
+     * @param shaderId The handle of the shader.
+     */
     private static void checkCompileStatus(int shaderId) {
         var status = glGetShaderi(shaderId, GL_COMPILE_STATUS);
         if (status == GL_FALSE) {
@@ -428,6 +495,9 @@ public class ShaderProgram implements Resource {
         }
     }
 
+    /**
+     * Links the shader program.
+     */
     private void linkAndValidateProgram() {
         // link
         glLinkProgram(id);
@@ -478,6 +548,11 @@ public class ShaderProgram implements Resource {
         // todo
     }
 
+    /**
+     * Searches for all uniforms in the shader code.
+     *
+     * @param shaderCode The shader code.
+     */
     private void findUniforms(String shaderCode) {
         final String uniformKeyword = "uniform";
 
@@ -543,6 +618,14 @@ public class ShaderProgram implements Resource {
         }
     }
 
+    /**
+     * Find all occurrences/positions of a sub string in a given string.
+     *
+     * @param text The given string.
+     * @param str The substring to search for.
+     *
+     * @return All positions.
+     */
     private ArrayList<Integer> findAllOccurances(String text, String str) {
         int index = 0;
 
@@ -561,6 +644,9 @@ public class ShaderProgram implements Resource {
         return positions;
     }
 
+    /**
+     * Create and store Uniform locations.
+     */
     private void addFoundUniforms() {
         for (var uniform : foundUniforms) {
             if (uniform.isUniformBlock) {
