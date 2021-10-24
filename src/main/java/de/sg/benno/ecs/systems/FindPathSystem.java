9a8da0dc326766bd545d7abe1e2d0c574f7d3e8e
stwe <no@mail.tt>
2021-10-24 19:05:47 +0200

    the ship gfx is changing by target

--- src/main/java/de/sg/benno/ecs/systems/FindPathSystem.java
+++ src/main/java/de/sg/benno/ecs/systems/FindPathSystem.java
@@ -24,9 +24,8 @@ import de.sg.benno.ai.Node;
 import de.sg.benno.chunk.TileGraphic;
 import de.sg.benno.content.Water;
 import de.sg.benno.debug.MousePicker;
-import de.sg.benno.ecs.components.PositionComponent;
-import de.sg.benno.ecs.components.SelectedComponent;
-import de.sg.benno.ecs.components.TargetComponent;
+import de.sg.benno.ecs.components.*;
+import de.sg.benno.ecs.core.Entity;
 import de.sg.benno.ecs.core.EntitySystem;
 import de.sg.benno.ecs.core.Signature;
 import de.sg.benno.input.Camera;
@@ -34,6 +33,8 @@ import de.sg.benno.ogl.renderer.SpriteRenderer;
 import de.sg.benno.ogl.resource.Texture;
 import de.sg.benno.renderer.Zoom;
 import de.sg.benno.state.Context;
+import org.joml.Vector2f;
+import org.joml.Vector3f;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -44,11 +45,19 @@ import static de.sg.benno.ogl.Log.LOGGER;
 
 /**
  * Represents a FindPathSystem.
- * This system iterates over all entities with the {@link PositionComponent} and the {@link SelectedComponent}.
+ * This system iterates over all entities with the {@link PositionComponent}, the {@link SelectedComponent},
+ * the {@link GfxIndexComponent} and {@link Ship4Component}.
  * The {@link TargetComponent} is added to the entity with the right mouse button.
  * If the {@link TargetComponent} exists, the path to the target is highlighted in color.
+ * The {@link GfxIndexComponent} is changed when the target changes.
  */
 public class FindPathSystem extends EntitySystem {
+
+    //-------------------------------------------------
+    // Constants
+    //-------------------------------------------------
+
+    private static final float HALF_ANGLE = 22.5f;
 
     //-------------------------------------------------
     // Member
@@ -133,7 +142,7 @@ public class FindPathSystem extends EntitySystem {
      */
     public FindPathSystem(Context context, Water water, Camera camera, Zoom currentZoom) throws Exception {
         this(context, water, camera, currentZoom, new Signature());
-        getSignature().setAll(PositionComponent.class, SelectedComponent.class);
+        getSignature().setAll(PositionComponent.class, SelectedComponent.class, GfxIndexComponent.class, Ship4Component.class);
     }
 
     //-------------------------------------------------
@@ -195,6 +204,8 @@ public class FindPathSystem extends EntitySystem {
                             targetComponent.targetWorldPosition = targetPosition;
                             targetComponent.path = path;
 
+                            updateDirection(entity);
+
                             LOGGER.debug("Change Ship target to x: {}, y: {}.", targetComponent.targetWorldPosition.x, targetComponent.targetWorldPosition.y);
                         } else {
                             try {
@@ -203,6 +214,8 @@ public class FindPathSystem extends EntitySystem {
                                     var targetComponent = targetComponentOptional.get();
                                     targetComponent.targetWorldPosition = targetPosition;
                                     targetComponent.path = path;
+
+                                    updateDirection(entity);
 
                                     LOGGER.debug("Add new Ship target x: {}, y: {}.", targetComponent.targetWorldPosition.x, targetComponent.targetWorldPosition.y);
                                 } else {
@@ -242,5 +255,99 @@ public class FindPathSystem extends EntitySystem {
         LOGGER.debug("Start clean up for the FindPathSystem.");
 
         spriteRenderer.cleanUp();
+    }
+
+    //-------------------------------------------------
+    // Helper
+    //-------------------------------------------------
+
+    /**
+     * Changes the ship's gfx index depending on the direction.
+     *
+     * @param entity An {@link Entity} object.
+     */
+    private void updateDirection(Entity entity) {
+        var targetComponentOptional = entity.getComponent(TargetComponent.class);
+        var ship4ComponentOptional = entity.getComponent(Ship4Component.class);
+        var gfxIndexComponentOptional = entity.getComponent(GfxIndexComponent.class);
+
+        if (targetComponentOptional.isPresent() && ship4ComponentOptional.isPresent() && gfxIndexComponentOptional.isPresent()) {
+            var target = targetComponentOptional.get().targetWorldPosition;
+            var ship = ship4ComponentOptional.get().ship4;
+
+            var d = new Vector2f(target).sub(new Vector2f(ship.getPosition()));
+            d.normalize();
+
+            var targetDirection = new Vector3f();
+
+            // direction vector
+            targetDirection.x = d.x;
+            targetDirection.y = d.y;
+
+            // store angle in z
+            var angle = Math.atan2(targetDirection.y, targetDirection.x);
+            var angleDeg = Math.toDegrees(angle) + 180.0;
+            targetDirection.z = (float)angleDeg;
+
+            // set new ship direction
+            ship.direction = getShipDirection(targetDirection.z);
+
+            // set new gfx index
+            gfxIndexComponentOptional.get().gfxIndex = ship.getCurrentGfxIndex();
+        }
+    }
+
+    /**
+     * Get a new ship direction.
+     *
+     * @param angleDeg An angle in degrees.
+     */
+    private int getShipDirection(float angleDeg) {
+        // 67.5 ... 112.5
+        if (angleDeg >= 45 + HALF_ANGLE && angleDeg <= 90 + HALF_ANGLE) {
+            return 0;
+        }
+
+        // 112.5 ... 157.5
+        if (angleDeg >= 90 + HALF_ANGLE && angleDeg <= 135 + HALF_ANGLE) {
+            return 1;
+        }
+
+        // 157.5 ... 202.5
+        if (angleDeg >= 135 + HALF_ANGLE && angleDeg <= 180 + HALF_ANGLE) {
+            return 2;
+        }
+
+        // 202.5 ... 247.5
+        if (angleDeg >= 180 + HALF_ANGLE && angleDeg <= 225 + HALF_ANGLE) {
+            return 3;
+        }
+
+        // 247.5 ... 292.5
+        if (angleDeg >= 225 + HALF_ANGLE && angleDeg <= 270 + HALF_ANGLE) {
+            return 4;
+        }
+
+        // 292.5 ... 337.5
+        if (angleDeg >= 270 + HALF_ANGLE && angleDeg <= 315 + HALF_ANGLE) {
+            return 5;
+        }
+
+        // 337.5 ... 360
+        if (angleDeg >= 315 + HALF_ANGLE && angleDeg <= 360) {
+            return 6;
+        }
+
+        // 0 ... 22.5
+        if (angleDeg >= 0 && angleDeg <= HALF_ANGLE) {
+            return 6;
+        }
+
+        // 22.5 ... 67.5
+        if (angleDeg >= HALF_ANGLE && angleDeg <= 45 + HALF_ANGLE) {
+            return 7;
+        }
+
+        return 0;
     }
 }
